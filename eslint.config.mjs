11d2199f23d8e1efedb8f50fc46a407ac0897267
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-03-26 16:38:19 +0700

    chore(deps): resolve all dependabot issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import prettier from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ['dist/**', 'build/**', 'node_modules/**'],
+    ignores: ['dist/**', 'build/**', 'lib/**', 'node_modules/**'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
